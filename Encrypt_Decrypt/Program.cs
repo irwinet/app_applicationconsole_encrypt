52c5d43382435c1002871e4ac762f1cf4177c13b
Irwin Estrada <ext_iestrada@tottus.com.pe>
2023-01-23 12:09:48 -0500

    Add Aes Encryt Decrypt

--- Encrypt_Decrypt/Program.cs
+++ Encrypt_Decrypt/Program.cs
@@ -33,11 +33,26 @@ namespace Encrypt_Decrypt
             Console.WriteLine(decryptedText);
             */
 
+            /*
             var rsaCryptoServiceProvider = new RSACryptoServiceProvider(2048);
             var cipherText = AsymmetricEncryptionDecryptionManager.Encrypt("Hola Mundo", rsaCryptoServiceProvider.ExportParameters(false));
             Console.WriteLine(cipherText);
             var plainText = AsymmetricEncryptionDecryptionManager.Decrypt(cipherText, rsaCryptoServiceProvider.ExportParameters(true));
             Console.WriteLine(plainText);
+            */
+
+            var key = "b14ca5898a4e4133bbce2ea2315a1916";
+
+            //Console.WriteLine("Please enter a secret key for the symmetric algorithm.");
+            //var key = Console.ReadLine();
+
+            Console.WriteLine("Please enter a string for encryption");
+            var str = Console.ReadLine();
+            var encryptedString = AesOperation.EncryptString(key, str);
+            Console.WriteLine($"encrypted string = {encryptedString}");
+
+            var decryptedString = AesOperation.DecryptString(key, encryptedString);
+            Console.WriteLine($"decrypted string = {decryptedString}");
 
             Console.ReadLine();
         }
